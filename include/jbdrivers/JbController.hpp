77907b32d8259b2fbb2d27a4725182ff74972a28
JohnnyB1290 <Strelok1290@gmail.com>
2019-12-26 11:06:09 +0300

    Add JbController

---
+++ include/jbdrivers/JbController.hpp
@@ -0,0 +1,63 @@
+/**
+ * @file
+ * @brief JbController class definition
+ *
+ *
+ * @note
+ * Copyright © 2019 Evgeniy Ivanov. Contacts: <strelok1290@gmail.com>
+ * All rights reserved.
+ * @note
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * @note
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @note
+ * This file is a part of JB_Lib.
+ */
+
+#ifndef JB_CONTROLLER_HPP_
+#define JB_CONTROLLER_HPP_
+
+#include <stdint.h>
+#include "jbkernel/jb_common.h"
+#include "jbkernel/JbKernel.hpp"
+#include "driver/gpio.h"
+
+typedef struct{
+    gpio_num_t pin;
+    gpio_mode_t direction;
+    gpio_pull_mode_t pullMode;
+}BoardGpio_t;
+
+namespace jblib
+{
+namespace jbdrivers
+{
+
+using namespace jbkernel;
+
+class JbController : public JbKernel
+{
+public:
+	static void initialize(void);
+	static void gpioOn(uint8_t number);
+	static void gpioOff(uint8_t number);
+	static void gpioTgl(uint8_t number);
+    static bool getGpio(uint8_t number);
+
+private:
+	static BoardGpio_t boardGpios_[];
+};
+
+}
+}
+
+#endif /*  JB_CONTROLLER_HPP_ */
---
+++ src/jbdrivers/JbController.cpp
@@ -0,0 +1,97 @@
+/**
+ * @file
+ * @brief JbController class realization
+ *
+ *
+ * @note
+ * Copyright © 2019 Evgeniy Ivanov. Contacts: <strelok1290@gmail.com>
+ * All rights reserved.
+ * @note
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * @note
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @note
+ * This file is a part of JB_Lib.
+ */
+
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+#include <string.h>
+#include "jbdrivers/JbController.hpp"
+
+namespace jblib
+{
+namespace jbdrivers
+{
+
+using namespace jbkernel;
+
+BoardGpio_t JbController::boardGpios_[] = JBCONTROLLER_BOARD_GPIOS;
+
+
+void JbController::initialize(void)
+{
+	static bool isInitialized = false;
+	if(!isInitialized) {
+        for(uint32_t i = 0; i < (sizeof(boardGpios_)/sizeof(BoardGpio_t)); i++) {
+            gpio_pad_select_gpio(boardGpios_[i].pin);
+            gpio_set_direction(boardGpios_[i].pin, boardGpios_[i].direction);
+            gpio_set_pull_mode(boardGpios_[i].pin,	boardGpios_[i].pullMode);
+        }
+		isInitialized = true;
+	}
+}
+
+
+
+void JbController::gpioOn(uint8_t number)
+{
+    gpio_set_level(boardGpios_[number].pin, 1);
+}
+
+
+
+void JbController::gpioOff(uint8_t number)
+{
+    gpio_set_level(boardGpios_[number].pin, 0);
+}
+
+
+
+void JbController::gpioTgl(uint8_t number)
+{
+    bool isOn = false;
+    if (boardGpios_[number].pin < 32) {
+        isOn = (GPIO.out >> boardGpios_[number].pin) & 1U;
+    } else {
+        isOn = (GPIO.out1.data >> boardGpios_[number].pin) & 1U;
+    }
+
+    if(isOn){
+        JbController::gpioOff(number);
+    }
+    else{
+        JbController::gpioOn(number);
+    }
+}
+
+
+bool JbController::getGpio(uint8_t number)
+{
+    printf("Minus pin: %u\n", boardGpios_[number].pin);
+    return gpio_get_level(boardGpios_[number].pin);
+}
+
+}
+}
