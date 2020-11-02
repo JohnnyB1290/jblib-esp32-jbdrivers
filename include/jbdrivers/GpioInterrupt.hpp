47f35a776cf063e5e09d4c3c12e78af73f479e04
JohnnyB1290 <Strelok1290@gmail.com>
2020-11-03 01:28:28 +0300

    Add Gpio Interupt driver

---
+++ include/jbdrivers/GpioInterrupt.hpp
@@ -0,0 +1,66 @@
+/**
+ * @file
+ * @brief GPIO Interrupt class definition
+ *
+ *
+ * @note
+ * Copyright © 2020 Evgeniy Ivanov. Contacts: <strelok1290@gmail.com>
+ * All rights reserved.
+ * @note
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * @note
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @note
+ * This file is a part of JB_Lib.
+ */
+
+#pragma once
+
+#include "jbkernel/jb_common.h"
+#include "jbkernel/callback_interfaces.hpp"
+#include "jbdrivers/JbController.hpp"
+
+namespace jblib {
+    namespace jbdrivers {
+
+        class GpioInterrupt
+        {
+        public:
+            struct Configuration
+            {
+                gpio_num_t pin = GPIO_NUM_NC;
+                gpio_pullup_t pullUp = GPIO_PULLUP_DISABLE;
+                gpio_pulldown_t pullDown = GPIO_PULLDOWN_DISABLE;
+                gpio_int_type_t edge = GPIO_INTR_ANYEDGE;
+            };
+
+            static void globalEnableGpioInterrupt(uint32_t intrFlags = ESP_INTR_FLAG_LOWMED);
+            static void globalDisableGpioInterrupt();
+
+            GpioInterrupt(const Configuration& config);
+            virtual ~GpioInterrupt();
+            void setCallback(::jblib::jbkernel::IVoidCallback* callback);
+            void resetCallback();
+            void enable();
+            void disable();
+
+        private:
+            static constexpr const char* logTag_ = "[ Gpio Intr ]";
+
+            ::jblib::jbkernel::IVoidCallback* callback_ = nullptr;
+            gpio_num_t pin_ = GPIO_NUM_NC;
+
+            static void isrHandler(void* arg);
+        };
+
+    }
+}
---
+++ src/jbdrivers/GpioInterrupt.cpp
@@ -0,0 +1,120 @@
+/**
+ * @file
+ * @brief GPIO Interrupt class realization
+ *
+ *
+ * @note
+ * Copyright © 2020 Evgeniy Ivanov. Contacts: <strelok1290@gmail.com>
+ * All rights reserved.
+ * @note
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * @note
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @note
+ * This file is a part of JB_Lib.
+ */
+
+#include <hal/gpio_types.h>
+#include <stdexcept>
+#include "jbdrivers/GpioInterrupt.hpp"
+
+using namespace ::jblib::jbkernel;
+using namespace ::jblib::jbdrivers;
+
+
+
+void GpioInterrupt::globalEnableGpioInterrupt(uint32_t intrFlags)
+{
+    auto ret = gpio_install_isr_service(intrFlags);
+    if(ret != ESP_OK){
+        ESP_LOGE(logTag_, "Install isr service error %i", ret);
+    }
+}
+
+
+
+void GpioInterrupt::globalDisableGpioInterrupt()
+{
+    gpio_uninstall_isr_service();
+}
+
+
+
+void IRAM_ATTR GpioInterrupt::isrHandler(void* arg)
+{
+    auto gpioInterrupt = reinterpret_cast<GpioInterrupt*>(arg);
+    gpio_set_level(GPIO_NUM_2, 1);
+    if(gpioInterrupt->callback_){
+        gpioInterrupt->callback_->voidCallback(arg, nullptr);
+    }
+    gpio_set_level(GPIO_NUM_2, 0);
+}
+
+
+
+GpioInterrupt::GpioInterrupt(const Configuration& config) : pin_(config.pin)
+{
+    gpio_config_t gpioConfig{};
+    gpioConfig.pin_bit_mask = 1ULL << static_cast<uint32_t>(config.pin);
+    gpioConfig.mode = GPIO_MODE_INPUT;
+    gpioConfig.pull_up_en = config.pullUp;
+    gpioConfig.pull_down_en = config.pullDown;
+    gpioConfig.intr_type = config.edge;
+    if(gpio_config(&gpioConfig) != ESP_OK){
+        throw std::logic_error("Gpio Config wrong parameters");
+    }
+    auto ret = gpio_isr_handler_add(config.pin, isrHandler, this);
+    if(ret == ESP_ERR_INVALID_STATE){
+        globalEnableGpioInterrupt();
+        ret = gpio_isr_handler_add(config.pin, isrHandler, this);
+    }
+    if(ret != ESP_OK){
+        throw std::logic_error("Add ISR handler error");
+    }
+    this->disable();
+}
+
+
+
+GpioInterrupt::~GpioInterrupt()
+{
+    gpio_isr_handler_remove(this->pin_);
+    gpio_reset_pin(this->pin_);
+}
+
+
+
+void GpioInterrupt::setCallback(::jblib::jbkernel::IVoidCallback* const callback)
+{
+    this->callback_ = callback;
+}
+
+
+
+void GpioInterrupt::resetCallback()
+{
+    this->callback_ = nullptr;
+}
+
+
+
+void GpioInterrupt::enable()
+{
+    gpio_intr_enable(this->pin_);
+}
+
+
+
+void GpioInterrupt::disable()
+{
+    gpio_intr_disable(this->pin_);
+}
